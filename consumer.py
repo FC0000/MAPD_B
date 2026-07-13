63c87ff6d61ede2a0f3187d553f09d8b93a7cdcf
FC0000 <126058608+FC0000@users.noreply.github.com>
2026-07-13 17:35:09 +0000

    updates

--- consumer.py
+++ consumer.py
@@ -13,8 +13,8 @@ BOOTSTRAP = "localhost:9092"
 
 FREQ_MIN_HZ = -1.1e6
 FREQ_MAX_HZ = 1.1e6
-POWER_MIN = 0
-POWER_MAX = 20
+POWER_MIN = 1e-4
+POWER_MAX = 30
 
 BENCHMARK_FILE = "benchmark_1.json"
 
@@ -58,8 +58,19 @@ global_n_averaged_scans = 0
 
 # =============================================================================
 # Main loop
-# =============================================================================
+# ============================================================================= 
 print(f"Listening for messages: latencies will be saved to {BENCHMARK_FILE}")
+
+n_eof = 0
+plot_start_time = None
+
+throughput = None
+n_scans_per_batch = None
+
+benchmark_records = []
+
+class eof_exit(Exception):
+    pass
 
 try:
     # Keep running as long as the matplotlib window is open
@@ -70,7 +81,21 @@ try:
 
         for _, messages in records.items():
             for msg in messages:
+                eof = False
                 receive_ts = time.time()
+
+                if msg.headers:
+                    for key, value in msg.headers:
+                        if key == "EOF":
+                            print("EOF")
+                            eof = True
+                            n_eof += 1
+                if n_eof != 0 and n_eof == n_workers:
+                    print(f"Received {n_eof} EOFs, exiting")
+                    raise eof_exit()
+                if eof:
+                     continue
+
                 packet = msg.value
     
                 worker_id = packet["worker_id"]
@@ -104,21 +129,21 @@ try:
                 worker["producer_tss"] = metrics["producer_timestamps"]
                 worker["waiting_times"] = metrics["waiting_times"]
                 worker["processing_times"] = metrics["processing_times"]
-                worker["scans_per_batch"] = metrics["scans_per_batch"]
-                worker["throughput"] = metrics["throughput"]
+                if n_scans_per_batch is None:
+                    n_scans_per_batch = metrics["scans_per_batch"]
+                else:
+                    assert n_scans_per_batch == metrics["scans_per_batch"]
+                if throughput is None:
+                    throughput = metrics["throughput"]
+                else:
+                    assert throughput == metrics["throughput"]
                 worker["receive_tss"].append(receive_ts)
             
-                # print(
-                #    f"{time.strftime('%H:%M:%S')} "
-                #    f"scans={worker['n_averaged_scans']}, "
-                #    f"producer: [{', '.join(f'{x:.1f}' for x in worker['producer_tss'])}], "
-                #    f"cpu-time: {np.sum(worker['processing_times']):.1f}ms, "
-                #    f"waits: [{', '.join(f'{x:.1f}' for x in worker['waiting_times'])}]s"
-                # )
                 
                 # Update global cumulative spectrum using a running average
                 if global_power_means is None:
-                    # First packet sets the baseline
+                    if plot_start_time is None:
+                        plot_start_time = time.time()
                     global_frequencies = worker["frequencies"]
                     global_power_means = worker["power_means"].copy()
                     global_power_M2s = worker["power_M2s"].copy()
@@ -160,18 +185,15 @@ try:
                     worker["last_fft_p95"] = 0.0
 
                 # Save Benchmark
-                if net_latencies_ms or fft_latencies:
-                    benchmark_record = {
-                        "scans_per_batch": worker["scans_per_batch"],
-                        "throughput": worker["throughput"],
-                        "timestamp": receive_ts,
+                benchmark_records.append({
                         "worker_id": worker_id,
-                        "num_scans": worker["n_averaged_scans"],
-                        "net_latencies_ms": net_latencies_ms,
-                        "fft_latencies_ms": fft_latencies
-                    }
-                    with open(BENCHMARK_FILE, "a") as f:
-                        f.write(json.dumps(benchmark_record) + "\n")
+                        "n_averaged_scans": worker["n_averaged_scans"],
+                        "production_tss": worker["producer_tss"],
+                        "fft_latencies_ms": fft_latencies,
+                        "waiting_times": worker["waiting_times"],
+                        "receive_tss": worker["receive_tss"],
+                    })
+                    
 
                 # Print on the dashboard
                 worker["log"].appendleft(
@@ -199,8 +221,10 @@ try:
 
             ax_global = fig.add_subplot(grid[0, :])
             global_line, = ax_global.plot([], [], lw=2)
+            global_error = ax_global.errorbar([], [], yerr=[], fmt="none", capsize=2, alpha=0.7 )
             ax_global.set_xlim(FREQ_MIN_HZ, FREQ_MAX_HZ)
             ax_global.set_ylim(POWER_MIN, POWER_MAX)
+            ax_global.set_yscale("log")
             ax_global.set_xlabel("Frequency (Hz)")
             ax_global.set_ylabel("Power")
             ax_global.grid(True)
@@ -212,6 +236,7 @@ try:
                 spectrum_line, = spectrum_axis.plot([], [], lw=1.5)
                 spectrum_axis.set_xlim(FREQ_MIN_HZ, FREQ_MAX_HZ)
                 spectrum_axis.set_ylim(POWER_MIN, POWER_MAX)
+                spectrum_axis.set_yscale("log")
                 spectrum_axis.grid(True)
                 spectrum_axis.set_xlabel("Frequency (Hz)")
                 spectrum_axis.set_ylabel("Power")
@@ -229,8 +254,21 @@ try:
 
         # Draw the latest data on the global plot
         if global_power_means is not None:
-            global_line.set_data(global_frequencies, global_power_means)
-            ax_global.set_title(f"Cumulative Mean Spectrum ({global_n_averaged_scans} scans)")
+            global_power_safe = np.maximum(global_power_means, 1e-12)
+
+            global_line.set_data(global_frequencies, global_power_safe)
+
+            # M2 -> standard deviation
+            if global_n_averaged_scans > 1:
+                variance = global_power_M2s / (global_n_averaged_scans - 1)
+
+                sigma = np.sqrt(variance)
+                global_error.remove()
+                global_error = ax_global.errorbar(global_frequencies, global_power_safe, yerr=sigma, fmt="none", capsize=2, alpha=0.7)
+
+            elapsed_time = time.time() - plot_start_time
+
+            ax_global.set_title(f"Cumulative Mean Spectrum ({global_n_averaged_scans} scans, elapsed {elapsed_time:.1f} s)")
 
         current_time = time.time()
         
@@ -239,8 +277,16 @@ try:
             worker = worker_states[w_id]
             plot = worker_plots[w_id]
 
-            plot["spectrum_line"].set_data(worker["frequencies"], worker["power_means"])
+            #plot["spectrum_line"].set_data(worker["frequencies"], worker["power_means"])
             plot["spectrum_axis"].set_title(f"Worker {w_id}")
+
+            freqs = worker["frequencies"]
+            power = worker["power_means"]
+            power = np.maximum(power, 1e-12)
+
+            freqs = worker["frequencies"]
+            power = np.maximum(worker["power_means"], 1e-12)
+            plot["spectrum_line"].set_data(freqs, power)
 
             age_seconds = current_time - worker["receive_tss"][-1]
             
@@ -268,6 +314,24 @@ try:
         # Render the updated graphics to the screen
         fig.canvas.draw_idle()
         fig.canvas.flush_events()
+except eof_exit:
+    pass
 finally:
+    finish_ts = time.time()
+
+    benchmark_output = {
+            "throughput": throughput,
+            "n_scans_per_batch": n_scans_per_batch,
+            "finish_ts": finish_ts,
+            "data": benchmark_records
+    }
+
+    with open(BENCHMARK_FILE, "w") as f:
+        f.write(json.dump(benchmark_output))
+
+    print(f"Benchmark written to {BENCHMARK_FILE}")
+
     # Close the Kafka connection
     consumer.close()
+
+input("Press Enter to exit...")
